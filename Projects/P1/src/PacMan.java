f45f6040ae5d301e9e710b94612bfb328cbd9869
UyenNguyen <upnguyen@terpmail.umd.edu>
2023-01-06 20:29:24 -0500

    1st implement of pacman-move

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -18,7 +18,26 @@ public class PacMan {
   }
 
   public boolean move() {
-    return false;
+    ArrayList<Location> smove = get_valid_moves();
+    
+    // PacMan not able to move
+    if (smove.isEmpty())
+    {
+    	return false;
+    }
+    else
+    {
+    	// Choose a random valid move
+    	Random rand = new Random();
+    	int getRanNum = rand.nextInt(smove.size());
+    	Location des = smove.get(getRanNum); 
+    	
+    	//Implement moving
+    	myLoc = des;
+    	myMap.move(myName, des, Map.Type.PACMAN);
+    	
+    	return true;
+    }
   }
 
   public boolean is_ghost_in_range() {
