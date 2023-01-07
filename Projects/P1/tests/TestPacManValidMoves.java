e6c59101e2fddf3da2a215edb7cf392ecec94316
Aaron Hetzer <adhetz@gmail.com>
2023-01-07 03:03:11 -0500

    Added mainframe, pacman and ghost objects. Haven't written any logic for the test itself

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -3,6 +3,24 @@ import junit.framework.*;
 
 public class TestPacManValidMoves extends TestCase {
 
+  // Create a map with Walls and Tokens
+  Mainframe frame = new MainFrame();
+
+  // Create PacMan
+  PacMan pacman = frame.addPacMan(new Location(5,10));
+
+
+  @Test
+  public void testAdjacentGhost(){
+    System.out.println("test adjacent ghost")
+
+    // Add ghost
+    Ghost ghost = frame.addGhost(new Location(6,10), "name", Color.red);
+
+    pacman.get_valid_moves()
+
+  }
+
   public void testPacManValidMoves() throws FileNotFoundException {
     return null;
   }
