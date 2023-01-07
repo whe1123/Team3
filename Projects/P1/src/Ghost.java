c63ab3633be17f98842d5704fda810cf2b97e9fe
UyenNguyen <upnguyen@terpmail.umd.edu>
2023-01-06 21:05:56 -0500

    1st implement of ghost-move

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -16,7 +16,26 @@ public class Ghost {
   }
 
   public boolean move() {
-    return false;
+    ArrayList<Location> smove = get_valid_moves();
+	    
+	    // PacMan not able to move
+	    if (smove.isEmpty())
+	    {
+	    	return false;
+	    }
+	    else
+	    {
+	    	// Choose a random valid move
+	    	Random rand = new Random();
+	    	int getRanNum = rand.nextInt(smove.size());
+	    	Location des = smove.get(getRanNum); 
+	    	
+	    	//Implement moving
+	    	myLoc = des;
+	    	myMap.move(myName, des, Map.Type.GHOST);
+	    	
+	    	return true;
+	    }
   }
 
   public boolean is_pacman_in_range() {
