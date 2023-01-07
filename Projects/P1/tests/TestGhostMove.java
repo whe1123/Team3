9be513c9e83efddaf6421ad91db34fa5376dab98
UyenNguyen <upnguyen@terpmail.umd.edu>
2023-01-07 14:58:21 -0500

    Test ghost-move

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -3,5 +3,17 @@ import junit.framework.*;
 
 public class TestGhostMove extends TestCase {
 
-  public void testGhostMove() throws FileNotFoundException {}
+  public void testGhostMove() throws FileNotFoundException {
+    NoFrame frame = new NoFrame(); // Create new map
+	  Ghost ghost = new Ghost("ghost", new Location(1, 3), frame.getMap());
+	  frame.addGhost(new Location(1,3), "ghost", Color.orange);
+	    
+	  frame.startGame();
+
+	  assertTrue(frame.getMap().getLoc(new Location(1, 3)).contains(Map.Type.GHOST));
+
+	  ghost.move();
+
+	  assertFalse(frame.getMap().getLoc(new Location(1, 3)).contains(Map.Type.GHOST));
+  }
 }
