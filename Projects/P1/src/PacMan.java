18a1f8c31ff61a2607139b62cdef1d1b71f5ac76
Dan Xu <danxu007@gmail.com>
2023-01-07 22:19:38 -0500

    Pacman - is_ghost_in_range

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -22,6 +22,24 @@ public class PacMan {
   }
 
   public boolean is_ghost_in_range() {
+
+    tempX = myLoc.x;
+    tempY = myLoc.y;
+
+    // Scrolls through all the X values
+    for(int i = tempX - 1; i <= tempX + 1; i++){
+
+	// Scrolls through all the Y values
+        for(int j = tempY - 1; j <= tempY + 1; j++){
+            Location tempLoc = new Location(tempX, tempY);
+	    
+	    // CHecks if the current location is a ghost
+	    if(myMap.getLoc(tempLoc) == Map.Type.GHOST){
+	        return true;
+	    }
+	}
+    }
+
     return false;
   }
 
