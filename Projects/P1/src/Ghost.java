d5c714f2ccd2ac004f603eef86a6403a591fd4af
Dan Xu <danxu007@gmail.com>
2023-01-07 23:43:36 -0500

    Pull request: ghost-is_pacman_in_range

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -20,6 +20,24 @@ public class Ghost {
   }
 
   public boolean is_pacman_in_range() {
+    
+    tempX = myLoc.x;
+    tempY = myLoc.y;
+
+    // Scrolls through all the X values
+    for(int i = tempX - 1; i <= tempX + 1; i++){
+        
+	//Scrolls through all the Y values
+	for(int j = tempY - 1; j <= tempY + 1; j++){
+	    Location tempLoc = new Location(i, j);
+
+	    // Checks if the current location is a pacman
+	    if(myMap.getLoc(tempLoc) == Map.Type.PACMAN){
+	        return true;
+	    }
+	}
+    }
+
     return false;
   }
 
