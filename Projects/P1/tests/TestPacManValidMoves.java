708d1d54fdd901ac7435440a2e77a19965ece09d
DanXu007 <121884367+DanXu007@users.noreply.github.com>
2023-01-10 14:54:44 -0500

    Revert "1st implementation of Map.move(String name, Location loc, Type type)"

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import junit.framework.*;
-import java.awt.Color;
 
 public class TestPacManValidMoves extends TestCase {
+
   public void testPacManValidMoves() throws FileNotFoundException {
     return null;
   }
