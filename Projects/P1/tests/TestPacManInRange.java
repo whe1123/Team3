00bc44c7d59c7176b07f81cb63fc689134cad161
Dan Xu <danxu007@gmail.com>
2023-01-10 22:43:05 -0500

    Added extra line

--- Projects/P1/tests/TestPacManInRange.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import junit.framework.*;
-
-public class TestPacManInRange extends TestCase {
-
-  public void testPacManInRange() throws FileNotFoundException {
-      return null;
-  }
-}
