956767c6b83a0ab2bfa53fedf62883b65b288b39
UyenNguyen <upnguyen@terpmail.umd.edu>
2023-01-11 20:38:02 -0500

    final Ghost

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+//import javax.tools.DocumentationTool.Location;
+
 public class Ghost {
   String myName;
   Location myLoc;
@@ -12,44 +14,45 @@ public class Ghost {
     this.myMap = map;
   }
 
-  /**  
-   * This method returns all the valid moves that a Ghost can 
-   * make given its current position. 
-   * 
-   * It is valid for a Ghost to move into a Cookie, Ghost, Empty
-   * or PacMan. 
-   * It is not valid for a Ghost to move into a Wall.
-   * 
-   * Note: This logic is similar to PacMan.get_valid_moves()
-   * 
-   * Method by adhetzer
-  */
   public ArrayList<Location> get_valid_moves() {
-    Location currentLoc = this.myLoc;
-    ArrayList<Location> validMoves;
-    validMoves = new ArrayList<Location>();
-
-    // There are eight locations surrounding a Ghost's current location
-    // So, we will check if any of these locations are a Map.Type.WALL.
-    // The outer for loop will be used to adjust the x-coordinate 
-    // to be checked. The inner for loop will be used to adjust the
-    // y-coordinate to be checked.
-    for (int x = -1; x < 2; x++) {
-      for (int y = -1; y < 2; y++) {
-        Location testLocation = currentLoc.shift(currentLoc.x + x, currentLoc.y + y);
-
-        // The only non-valid move for a Ghost is going through a WALL
-        if (!(this.myMap.getLoc(testLocation).contains(Map.Type.WALL))) {
-          validMoves.add(testLocation);
-        }
-      }
-    }
-    
-    return validMoves;
+	  ArrayList<Location> setOfMove = new ArrayList<Location>();
+		
+		if (myMap.getLoc(myLoc.shift(0, 1)) != null)
+		{
+			if (!myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.WALL))
+			{
+				setOfMove.add(myLoc.shift(0, 1));
+			}
+		}
+		
+		if (myMap.getLoc(myLoc.shift(0, -1)) != null)
+		{
+			if (!myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.WALL))
+			{
+				setOfMove.add(myLoc.shift(0, -1));
+			}
+		}
+		
+		if (myMap.getLoc(myLoc.shift(1, 0)) != null)
+		{
+			if (!myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.WALL))
+			{
+				setOfMove.add(myLoc.shift(1, 0));
+			}
+		}
+		
+		if (myMap.getLoc(myLoc.shift(-1, 0)) != null)
+		{
+			if (!myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.WALL))
+			{
+				setOfMove.add(myLoc.shift(-1, 0));
+			}
+		}
+	    return setOfMove;
   }
 
   public boolean move() {
-    ArrayList<Location> smove = get_valid_moves();
+	  ArrayList<Location> smove = get_valid_moves();
 	    
 	    // PacMan not able to move
 	    if (smove.isEmpty())
@@ -72,56 +75,37 @@ public class Ghost {
   }
 
   public boolean is_pacman_in_range() {
-    
-    int tempX = myLoc.x;
-    int tempY = myLoc.y;
+	  int tempX = myLoc.x;
+	  int tempY = myLoc.y;
+	  
+	  // Scrolls through all the X values
+	  for(int i = tempX - 1; i <= tempX + 1; i++)
+	  {
+		  // Scrolls through all the Y values
+		  for(int j = tempY - 1; j <= tempY + 1; j++)
+		  {
+			  Location tempLoc = new Location(i, j);
+			  // Checks if the current location is a ghost
 
-    // Scrolls through all the X values
-    for(int i = tempX - 1; i <= tempX + 1; i++){
-        
-	//Scrolls through all the Y values
-	for(int j = tempY - 1; j <= tempY + 1; j++){
-	    Location tempLoc = new Location(i, j);
-
-	    // Checks if the current location is a pacman
-	    if(myMap.getLoc(tempLoc).contains(Map.Type.PACMAN)){
-	        return true;
-	    }
-	}
-    }
-
-    return false;
+			  if((myMap.getLoc(tempLoc) != null) 
+					  && ((myMap.getLoc(tempLoc).contains(Map.Type.PACMAN))
+							  || (myMap.getLoc(myLoc)).contains(Map.Type.PACMAN)))
+			  {
+				  return true;
+				  }
+			  }
+		  }
+	  return false;
   }
 
+
+  
   public boolean attack() {
-    // Based on Dan Xu's method for is_pacman_in_range
-    if (!is_pacman_in_range()) {
-      return false;
+    if (is_pacman_in_range())
+    {
+    	myMap.attack(myName);
+    	return true;
     }
-    else{
-      int v1 = myLoc.x;
-      int v2 = myLoc.y;
-  
-      // Scrolls through all the X values
-      for(int i = v1 - 1; i <= v1 + 1; i++){
-        System.out.println("Test1");
-        //Scrolls through all the Y values
-        for(int j = v2 - 1; j <= v2 + 1; j++){
-            Location tempLoc = new Location(i, j);
-          System.out.println("Test1");
-            // Checks if the current location is a pacman
-            if(myMap.getLoc(tempLoc).contains(Map.Type.PACMAN)){
-                myMap.move(myName, tempLoc, Map.Type.GHOST);
-                System.out.println("Test3");
-                return true;
-            }
-        }
-      }
-  
-      return false;
-    }
-
+    return false;
+  }
 }
-
-}
-
