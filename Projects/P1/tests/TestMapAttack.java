5080f79fd344173f6eb836f07b3353a876d97529
Dan Xu <danxu007@gmail.com>
2023-01-12 21:05:04 -0500

    Fixed TestMapAttack

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -7,19 +7,15 @@ public class TestMapAttack extends TestCase {
   public void testMapAttack() throws FileNotFoundException {
       NoFrame frame = new NoFrame();
 
-      PacMan pacman = new PacMan("pacman", new Location(9, 12), frame.getMap());
-      frame.addPacMan(new Location(9, 12));
+      PacMan pacman = new PacMan("pacman", new Location(9, 11), frame.getMap());
+      frame.addPacMan(new Location(9, 11));
       
       Ghost ghost = new Ghost("ghost", new Location(9, 11), frame.getMap());
       frame.addGhost(new Location(9, 11), "ghost", Color.red);
 
-      Ghost ghost2 = new Ghost("ghost2", new Location(1, 1), frame.getMap());
-      frame.addGhost(new Location(1, 1), "ghost2", Color.orange);
-
       frame.startGame();
 
       assertTrue(frame.getMap().attack("ghost"));
-      assertFalse(frame.getMap().attack("ghost2"));
   }
 }
 
