3fcf6012a70b0fcb4b56575f4e4b3bb469f223ca
Aman Thanvi <22306104+amanthanvi@users.noreply.github.com>
2023-01-17 19:04:30 -0500

    Fixed ghost-is_pacman_in_range

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -77,24 +77,22 @@ public class Ghost {
   public boolean is_pacman_in_range() {
 	  int tempX = myLoc.x;
 	  int tempY = myLoc.y;
-	  
-	  // Scrolls through all the X values
-	  for(int i = tempX - 1; i < tempX + 1; i++)
-	  {
-		  // Scrolls through all the Y values
-		  for(int j = tempY - 1; j < tempY + 1; j++)
-		  {
-			  Location tempLoc = new Location(j, j);
-			  // Checks if the current location is a pacman
 
-			  if(((myMap.getLoc(tempLoc).contains(Map.Type.GHOST))
-							  || (myMap.getLoc(myLoc)).contains(Map.Type.GHOST)))
-			  {
-				  return true;
-				  }
-			  }
-		  }
-	  return false;
+	  // Scrolls through all the X values
+	  for(int i = tempX - 1; i <= tempX + 1; i++){
+		
+		//Scrolls through all the Y values
+		for(int j = tempY - 1; j <= tempY + 1; j++){
+        	Location tempLoc = new Location(i, j);
+
+        // Checks if the current location is a pacman
+        if(myMap.getLoc(tempLoc).contains(Map.Type.PACMAN)){
+            return true;
+        }
+    }
+    }
+
+    return false;
   }
 
 
